a9208a4772761ed4b49d7ea5f91235344f94a2bf
Joey van de Looverbosch <joeyvandelooverbosch@hotmail.com>
2023-03-06 14:11:02 +0100

    Finished project overview

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
     <title>Portfolio 2023</title>
 </head>
 <body>
+<div class="custom-cursor">
+	<div class="cursor cursor--large"></div>
+	<div class="cursor cursor--small"></div>
+</div>
     <div class="custom-modal">
         <div class="modal-wrapper">
             <div class="row row-lead">
@@ -44,10 +48,107 @@
         </div>
     </div>
 
+
+
+    <div class="invidvidual-project-visual col-12">
+        <div class="content">
+            <p>Learn more about the</p>
+            <h2>INDIVIDUAL PROJECT</h2>
+            <br>
+            <p>
+            Check out my personal portfolio project, created as part of a 4-week school project. I used the double diamond method to design, research, prototype, realize, and validate the website. 
+            <br><br>
+            The result is a user-friendly and visually appealing showcase of my skills and experience. Use the image slider to get a feeling of the proces that was undertaken.
+            </p>
+            <br>
+            <svg class="arrows">
+                <path class="a1" d="M0 0 L30 32 L60 0"></path>
+                <path class="a2" d="M0 20 L30 52 L60 20"></path>
+                <path class="a3" d="M0 40 L30 72 L60 40"></path>
+            </svg>
+        </div>
+        <div id="app">
+        <div class="slider--container">
+            <div class="slider__wrapper">
+                <div class="slider__btn--container">
+                    <button class="btn prev">Prev</button>
+                </div>
+
+                <div class="slider">
+                    <div class="slides__wrapper">
+                        <div class="slide">
+                            <div class="slide__img">
+                                <img src="src/mockup1.png" />
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <div class="slide__img">
+                                <img src="src/mockup1.png" />
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <div class="slide__img">
+                                <img src="https://source.unsplash.com/-heLWtuAN3c" />
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <div class="slide__img">
+                                <img src="https://source.unsplash.com/PP8Escz15d8" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="transition-slide"></div>
+                </div>
+
+                <div class="slider__btn--container">
+                    <button class="btn next">Next</button>
+                </div>
+            </div>
+
+            <div class="marquee__wrapper">
+                <div class="marquees">
+                    <div class="marquee">
+                        <div class="item" data-word="DEFINE" data-number="01"></div>
+                        <div class="item" data-word="DEFINE" data-number="01"></div>
+                        <div class="item" data-word="DEFINE" data-number="01"></div>
+                    </div>
+
+                    <div class="marquee">
+                        <div class="item" data-word="RIVER" data-number="02"></div>
+                        <div class="item" data-word="RIVER" data-number="02"></div>
+                        <div class="item" data-word="RIVER" data-number="02"></div>
+                    </div>
+
+                    <div class="marquee">
+                        <div class="item" data-word="VALLEY" data-number="03"></div>
+                        <div class="item" data-word="VALLEY" data-number="03"></div>
+                        <div class="item" data-word="VALLEY" data-number="03"></div>
+                    </div>
+
+                    <div class="marquee">
+                        <div class="item" data-word="FOREST" data-number="04"></div>
+                        <div class="item" data-word="FOREST" data-number="04"></div>
+                        <div class="item" data-word="FOREST" data-number="04"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="slider__progress--container">
+                <span class="slide-num current-slide">01</span>
+                <div class="progress">
+                    <span></span>
+                </div>
+                <span class="slide-num">04</span>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
     <div class="individual-project col-12">
         <div class="row">
             <div class="col-8 d-flex lead-wrapper">
-                <p class="lead">INDIVIDUAL PROJECT</p>
+                <p class="lead">INDIVIDUAL PROJECT EVIDENCE</p>
                 <h2 class="title">Creation of a professional portfolio website</h2>
             </div>
 
@@ -87,7 +188,7 @@
                     <div class="content">
                         <p class="lead">Discovering the issue</p>
                         <ul>
-                            <li class="deliverable"><i class="fa-brands fa-github mr-2"></i>GIT Repository</li>
+                            <li><a target="_blank" href="https://github.com/joeyvdlooverbosch/Portfolio_2023"><i class="fa-brands fa-github mr-2"></i>GIT Repository</a></li>
                             <li class="deliverable"><i class="fa-solid fa-desktop mr-2"></i>Portfolio website</li>
                         </ul>
                     </div>
@@ -223,24 +324,32 @@
 
     <div class="contact col-12">
         <div class="row">
-            <div class="col-12 content">
+            <!-- <div class="col-12 content">
                 <h2>Contact me</h2>
                 <p>Use the button below to send me an email.</p>
                 <a href="#" class="email">Contact</a>
-            </div>
-
+            </div> -->
         </div>
     </div>
-    </div>
+</div>
 
-
+<div class="loading__wrapper">
+	<div class="loader--text">Loading...</div>
+	<div class="loader">
+		<span></span>
+	</div>
+</div>
   
 </body>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js"></script>
+<script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/less"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="js/main.js"></script>
+<script src="js/grained.js"></script>
+<script src="js/image_handler.js"></script>
 </html>
